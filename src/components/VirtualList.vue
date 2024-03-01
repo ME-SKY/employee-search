209344e0a21bc83a5dcf682e7e6b8de973ac8179
ME-SKY <meskalitofly@gmail.com>
2024-03-02 00:36:54 +0700

    arrange code

--- src/components/VirtualList.vue
+++ src/components/VirtualList.vue
@@ -1,26 +1,16 @@
-<template>
-  <div class="virtual-list" :style="listStyle">
-    <div class="virtual-list-phantom" :style="phantomStyle"></div>
-    <div class="virtual-list-content" :style="contentStyle">
-      <slot v-for="item in visibleItems" :item="item"></slot>
-    </div>
-  </div>
-</template>
-
 <script setup>
 import { ref, onMounted, onUnmounted, computed, watch } from 'vue';
 
 const props = defineProps({
-  items: Array,
-  itemHeight: Number,
-  height: Number
+    items: Array,
+    itemHeight: Number,
+    height: Number
 });
-
 const state = ref({
-  startOffset: 0,
-  endOffset: 0,
-  visibleItems: [],
-  scrollTop: 0
+    startOffset: 0,
+    endOffset: 0,
+    visibleItems: [],
+    scrollTop: 0
 });
 
 const listEl = ref(null);
@@ -29,54 +19,68 @@ const visibleCount = computed(() => Math.ceil(props.height / props.itemHeight));
 const totalHeight = computed(() => props.items.length * props.itemHeight);
 
 const listStyle = computed(() => ({
-  height: `${props.height}px`,
-  overflowY: 'auto'
+    height: `${props.height}px`,
+    overflowY: 'auto'
 }));
 
 const phantomStyle = computed(() => ({
-  height: `${totalHeight.value}px`,
-  position: 'relative'
+    height: `${totalHeight.value}px`,
+    position: 'relative'
 }));
 
 const contentStyle = computed(() => ({
-  position: 'absolute',
-  top: `${state.value.startOffset}px`
+    position: 'absolute',
+    top: `${state.value.startOffset}px`
 }));
 
 const updateVisibleItems = () => {
-  const scroll = listEl.value.scrollTop;
-  const startIndex = Math.floor(scroll / props.itemHeight);
-  const endIndex = startIndex + visibleCount.value;
-  state.value.startOffset = startIndex * props.itemHeight;
-  state.value.endOffset = (props.items.length - endIndex) * props.itemHeight;
-  state.value.visibleItems = props.items.slice(startIndex, endIndex);
+    const scroll = listEl.value.scrollTop;
+    const startIndex = Math.floor(scroll / props.itemHeight);
+    const endIndex = startIndex + visibleCount.value;
+    state.value.startOffset = startIndex * props.itemHeight;
+    state.value.endOffset = (props.items.length - endIndex) * props.itemHeight;
+    state.value.visibleItems = props.items.slice(startIndex, endIndex);
 };
 
 const onScroll = () => {
-  state.value.scrollTop = listEl.value.scrollTop;
-  updateVisibleItems();
+    state.value.scrollTop = listEl.value.scrollTop;
+    updateVisibleItems();
 };
 
 onMounted(() => {
-  listEl.value.addEventListener('scroll', onScroll);
-  updateVisibleItems();
+    listEl.value.addEventListener('scroll', onScroll);
+    updateVisibleItems();
 });
 
 onUnmounted(() => {
-  listEl.value.removeEventListener('scroll', onScroll);
+    listEl.value.removeEventListener('scroll', onScroll);
 });
 
 watch(() => props.items, updateVisibleItems, { deep: true });
 </script>
 
+<template>
+    <div class="virtual-list" :style="listStyle">
+        <div class="virtual-list-phantom" :style="phantomStyle"></div>
+        <div class="virtual-list-content" :style="contentStyle">
+            <slot v-for="item in visibleItems" :item="item"></slot>
+        </div>
+    </div>
+</template>
+
+
+
+
 <style scoped>
 .virtual-list {
-  position: relative;
+    position: relative;
 }
+
 .virtual-list-phantom {
-  will-change: transform;
+    will-change: transform;
 }
+
 .virtual-list-content {
-  will-change: transform;
+    will-change: transform;
 }
 </style>
